89e21cd022e52f9933884c415935057bd7d3e579
David <davidmolinamorales1999@gmail.com>
2021-04-09 19:19:37 +0200

    Update honami_R.sh

--- honami_R.sh
+++ honami_R.sh
@@ -1,12 +1,12 @@
 ## Build
-  repo init -u https://github.com/AICP/platform_manifest.git -b r11.1
+  repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
   repo sync
   source build/envsetup.sh
 
 
  #Repos
- git clone -b havoc-11.1 https://github.com/daviiid99/android_device_sony_honami device/sony/honami
- git clone -b r11.1 https://github.com/AICP/device_sony_rhine-common.git device/sony/rhine-common
+ git clone -b lineage-18.1 https://github.com/daviiid99/android_device_sony_honami device/sony/honami
+ git clone -b r11.1 https://github.com/daviiid99/device_sony_rhine-common device/sony/rhine-common
  git clone -b r11.1 https://github.com/daviiid99/device_sony_msm8974-common device/sony/msm8974-common
  git clone -b r11.1 https://github.com/daviiid99/kernel_sony_msm8974.git kernel/sony/msm8974
  git clone -b r11.1 https://github.com/AICP/proprietary_vendor_sony.git vendor/sony
@@ -14,13 +14,9 @@
  git clone -b q10.0 https://github.com/AICP/hardware_sony_macaddrsetup.git hardware/sony/macaddrsetup
  git clone -b q10.0 https://github.com/AICP/hardware_sony_thermanager.git hardware/sony/thermanager
  git clone -b q10.0 https://github.com/AICP/hardware_sony_timekeep.git hardware/sony/timekeep
- git clone -b q10.0 https://github.com/AICP/external_sony_boringssl-compat.git external/sony/boringssl-compat
+ git clone -b q10.0 https://github.com/AICP/external_sony_boringssl-compat.git external/sony/boringssl-compat 
  
  #Picks
- repopick -f 295701 # Add product_launched_with_j_mr2 for JellyBean 4.3
- repopick 292788 # adb
- repopick -g https://gerrit.aicp-rom.com 104259 # rhine-common
- repopick -g https://gerrit.aicp-rom.com 106145 106147 # 8974-common
  repopick -g https://gerrit.aicp-rom.com 105818 -P system/core
  repopick -g https://gerrit.aicp-rom.com 105646 -P frameworks/native
  repopick -g https://gerrit.aicp-rom.com 105644 -P build/make
@@ -28,5 +24,6 @@
  repopick -g https://gerrit.aicp-rom.com 105817 -P vendor/lineage
  
  ## Build
+lunch lineage_honami-userdebug
 brunch honami
 
