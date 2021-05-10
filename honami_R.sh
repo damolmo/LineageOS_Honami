855c715eb1ffaa29e4605671e75761500be8e101
David <davidmolinamorales1999@gmail.com>
2021-05-10 14:12:54 +0200

    Updated instructions with clean patches

--- honami_R.sh
+++ honami_R.sh
@@ -1,8 +1,7 @@
-## Build
-  repo init -u git://github.com/daviiid99/android -b lineage-18.1
+#Sync
+  repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
   repo sync
   source build/envsetup.sh
-
 
  #Repos
  git clone -b lineage-18.1 https://github.com/daviiid99/android_device_sony_honami device/sony/honami
@@ -15,15 +14,19 @@
  git clone -b q10.0 https://github.com/AICP/hardware_sony_thermanager.git hardware/sony/thermanager
  git clone -b q10.0 https://github.com/AICP/hardware_sony_timekeep.git hardware/sony/timekeep
  git clone -b q10.0 https://github.com/AICP/external_sony_boringssl-compat.git external/sony/boringssl-compat 
+ git clone -b patches https://github.com/daviiid99/LineageOS_Honami.git RPatches/
  
- #Picks
- repopick -g https://gerrit.aicp-rom.com 105818 -P system/core
- repopick -g https://gerrit.aicp-rom.com 105646 -P frameworks/native
- repopick -g https://gerrit.aicp-rom.com 105644 -P build/make
- repopick -g https://gerrit.aicp-rom.com 105645 -P build/soong
- repopick -g https://gerrit.aicp-rom.com 105817 -P vendor/lineage
+ #Patches
+ patch -d system/core -p1 <  RPatches/system_core/android_bp.patch
+ patch -d system/core -p1 <  RPatches/system_core/service_cpp.patch
+ patch -d frameworks/native -p1 <  RPatches/frameworks_native/libs/renderengine/android_bp.patch
+ patch -d frameworks/native -p1 <  RPatches/frameworks_native/libs/renderengine/gl/filters/blurfilter_cpp.patch
+ patch -d build/make -p1 <  RPatches/build_make_core/soong_config_mk.patch
+ patch -d build/soong -p1 <  RPatches/build_soong_android/variable_go.patch
+ patch -d vendor/lineage -p1 <  RPatches/vendor_lineage/build_soong/android_bp.patch
+ patch -d vendor/lineage -p1 <  RPatches/vendor_lineage/config/BoardConfigSoong_mk.patch
  
- ## Build
+ #Build
 lunch lineage_honami-userdebug
 brunch honami
 
