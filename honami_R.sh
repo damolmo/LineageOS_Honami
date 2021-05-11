211883e143fbfc674bbd620bf0fc0548c4b3bb63
David <davidmolinamorales1999@gmail.com>
2021-05-11 14:34:02 +0200

    Update honami_R.sh

--- honami_R.sh
+++ honami_R.sh
@@ -1,7 +1,36 @@
+#Dependencies
+sudo apt update&&sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev python
+
+#Install latest repo
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+#Install Android SDK Platform Tools
+wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip platform-tools-latest-linux.zip -d ~
+cat >> ~/.profile<< EOF
+if [ -d "\$HOME/platform-tools" ] ; then
+    PATH="\$HOME/platform-tools:\$PATH"
+fi
+EOF
+
+#Set build cache to 50G
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
+source ~/.bashrc
+ccache -M 50G
+
+#Git config
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name
+
 #Sync
-  repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
-  repo sync
-  source build/envsetup.sh
+mkdir -p ~/android/lineage
+repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
+repo sync
+source build/envsetup.sh
 
  #Repos
  git clone -b lineage-18.1 https://github.com/daviiid99/android_device_sony_honami device/sony/honami
