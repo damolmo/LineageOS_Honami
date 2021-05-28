7c74cde98b12f27da574f800eb77ca49b770060b
David <davidmolinamorales1999@gmail.com>
2021-05-28 14:28:07 +0200

    Update honami_R.sh

--- honami_R.sh
+++ honami_R.sh
@@ -40,7 +40,7 @@ source build/envsetup.sh
  git clone -b r11.1 https://github.com/daviiid99/kernel_sony_msm8974.git kernel/sony/msm8974
  git clone -b r11.1 https://github.com/AICP/proprietary_vendor_sony.git vendor/sony
  git clone -b r11.1-caf https://github.com/AICP/device_sony_common.git device/sony/common-caf
- git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_sony_macaddrsetupt.git hardware/sony/macaddrsetup
+ git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_sony_macaddrsetup.git hardware/sony/macaddrsetup
  git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_sony_thermanager.git hardware/sony/thermanager
  git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep
  git clone -b lineage-18.1 https://github.com/LineageOS/android_external_sony_boringssl-compat.git external/sony/boringssl-compat 
