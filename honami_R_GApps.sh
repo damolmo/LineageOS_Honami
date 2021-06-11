fd9b5ff579c8b07916d9e2602f3dbf5d760cff80
David <davidmolinamorales1999@gmail.com>
2021-06-12 01:02:48 +0200

    Updated instructions
    All files are merged in the same file

--- honami_R_GApps.sh
+++ honami_R_GApps.sh
@@ -56,27 +56,7 @@ source build/envsetup.sh
  wget https://github.com/daviiid99/daviiid99/releases/download/honami/product.zip
  unzip product.zip  # GApps base package
  rm product.zip
- cd product/priv-app
- 
- mkdir Gcam&&cd Gcam # Compatible 32-bit GCAM
- wget https://github.com/daviiid99/daviiid99/releases/download/honami/Gcam.apk
- cd ..
- 
- mkdir Play&&cd Play # Google Play Store, missing from base package
- wget https://github.com/daviiid99/daviiid99/releases/download/honami/Play.apk
- cd ..
- 
- mkdir Google&&cd Google # Google app, missing from base package
- wget https://github.com/daviiid99/daviiid99/releases/download/honami/Google.apk
- cd ..
- 
- mkdir soundpicker&&cd soundpicker # Google Sound Picker app, solves missing sounds
- wget https://github.com/daviiid99/daviiid99/releases/download/honami/soundpicker.apk
- cd ..
- 
- mkdir Wallpaper&&cd Wallpaper #Google Wallpaper app, solves missing wallpaper app with Pixel Launcher
- wget https://github.com/daviiid99/daviiid99/releases/download/honami/Wallpaper.apk
- cd ../../../../../../../../
+ cd ../../../../../
  
 #Build
 brunch honami
