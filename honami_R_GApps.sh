3f3ed899309da9ebf22b07b0f0f50eb6d7d62820
David <davidmolinamorales1999@gmail.com>
2021-07-02 13:09:33 +0200

    New patch (needs testing)

--- honami_R_GApps.sh
+++ honami_R_GApps.sh
@@ -50,6 +50,7 @@ source build/envsetup.sh
  patch -d packages/apps/Settings -p1 <  RPatches/Display_NFC_Settings.patch #Fixes missing NFC icon in Settings
  patch -d vendor/qcom/opensource/dataservices -p1 <  RPatches/RMNET_NETLINK_NEW_VND_WITH_PREFIX.patch #Fixes build process
  patch -d frameworks/base -p1 < RPatches/Disable_Wallpaper_Zoom.patch #Fixes Android R Wallpaper Zoom
+ patch -d vendor/lineage -p1 < RPatches/Whitelist_priv-app_Permissions.patch #Fixes priv-app permissions 
   
 #Prebuilt GMS
  mkdir -p out/target/product/honami/system/product
